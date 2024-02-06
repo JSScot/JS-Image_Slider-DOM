2bd89eb59fe65881f0eff7b2660c13bac3447ced
JSScot <153024062+JSScot@users.noreply.github.com>
2024-02-06 12:48:04 +0000

    Good for mobile

--- script.js
+++ script.js
@@ -1,75 +1,57 @@
 console.log("Helloooo, testinggg")
 
-// select arrows 
+// Select arrows
 const prevArrow = document.querySelector('.prev');
-const nextArrow = document.querySelector('.next'); 
+const nextArrow = document.querySelector('.next');
 
-
-// number of imgs in the container
-
-
-// insert imgs into the container
-
-const updateBgImage = () => {
-  
-}
-
-
-
-
-
-
-// save number of images in container
+// Reference to the carousel items
 const carouselContainer = document.querySelector('.carousel').children;
-let totalSlides = carouselContainer.length;
-let index = 0;
+let totalSlides = carouselContainer.length; // The number of children
+let index = 0; // Initial index
 
+// Function to update background images for carousel items
+const updateBackgroundImage = () => {
+  for (let i = 0; i < totalSlides; i++) {
+    const imageUrl = carouselContainer[i].getAttribute('data-image');
+    carouselContainer[i].style.backgroundImage = `url('${imageUrl}')`;
+  }
+};
 
+// Call the function to set background images on initial load
+updateBackgroundImage();
+
+// Add event listener to the arrows
 prevArrow.addEventListener('click', () => {
-// add console log to test it workds 
-moveSlides("prev");
+  moveSlides("prev");
 });
 
 nextArrow.addEventListener('click', () => {
-moveSlides("next");
+  moveSlides("next");
 });
 
+// Function to move slides
 const moveSlides = (direction) => {
-
-  if(direction === 'next') {
+  if (direction === 'next') {
     index++;
-    if(index === totalSlides) {
-      index = 0;
+    if (index === totalSlides) {
+      index = 0; // Loop back to the first image
     }
   } else {
-    if(index === 0) {
-      index = totalSlides - 1;
+    if (index === 0) {
+      index = totalSlides - 1; // Move to the last image
     } else {
       index--;
     }
   }
 
-  for(let i = 0; i < totalSlides; i++) {
+  // Update the 'main' class for the current slide
+  for (let i = 0; i < totalSlides; i++) {
     carouselContainer[i].classList.remove('main');
   }
-
   carouselContainer[index].classList.add('main');
-  console.log(index);
-
 };
 
+// Automatically move to the next slide every 2000ms (2 seconds)
 setInterval(() => {
-console.log('setInterval is working every two seconds')
   moveSlides('next');
 }, 2000);
-
-
-
-
-
-
-
-    
-
-
-
